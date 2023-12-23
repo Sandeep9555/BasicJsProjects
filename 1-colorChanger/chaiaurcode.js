a8b97db68f811c653ed774785cac5f8a6a1cdcb6
Sandeep9555 <sandeepsingg6292@gmail.com>
2023-12-23 13:35:22 +0530

    project1 commit

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,2 +1,27 @@
-const button = document.querySelectorAll('.button');
-console.log(button);
+const button = document.querySelectorAll('.button')
+const body=document.querySelector('body');
+button.forEach(function(button){
+    //console.log(button);
+    button.addEventListener('click',function(e){
+        
+        if(e.target.id==='grey')
+         body.style.backgroundColor=e.target.id;
+    })
+    button.addEventListener('click',function(e){
+        
+        if(e.target.id==='red')
+         body.style.backgroundColor=e.target.id;
+    })
+    button.addEventListener('click',function(e){
+        
+        if(e.target.id==='blue')
+         body.style.backgroundColor=e.target.id;
+    })
+    button.addEventListener('click',function(e){
+        
+        if(e.target.id==='yellow')
+         body.style.backgroundColor=e.target.id;
+    })
+
+});
+
